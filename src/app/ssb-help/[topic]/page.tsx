59ff52c8fa610e61e56388f237d95bdb6b862834
anurag_chidanand <agandikote@gmail.com>
2025-05-04 15:46:39 +0530

    Fix: ssbhelp fix.

--- src/app/ssb-help/[topic]/page.tsx
+++ src/app/ssb-help/[topic]/page.tsx
@@ -1,10 +1,9 @@
-export default function SSBHelpPage({ params }: { params: { topic: string } }) {
-    return (
-        <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold text-red-900 mb-6">
-                {decodeURIComponent(params.topic).replace(/-/g, ' ')}
-            </h1>
-            {/* Add SSB help topic specific content */}
-        </div>
-    )
+import React from 'react'
+
+const SSBHelpPage = () => {
+  return (
+    <div>SSBHelpPage</div>
+  )
 }
+
+export default SSBHelpPage
