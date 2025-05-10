03b50fc170197ffc5cf35f11963c4fe95a1117a7
anurag_chidanand <agandikote@gmail.com>
2025-05-10 16:32:43 +0530

    fix: error

--- src/app/branches/[location]/page.tsx
+++ src/app/branches/[location]/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 type Props = {
     params: {
         location: string;
@@ -9,12 +7,13 @@ type Props = {
 export const dynamic = 'force-dynamic';
 
 export default function BranchPage({ params }: Props) {
+
+    console.log('params:', params);
     return (
         <div className="container mx-auto px-4 py-8">
             <h1 className="text-3xl font-bold text-red-900 mb-6">
                 {decodeURIComponent(params.location)} Branch
             </h1>
-            {/* Add branch specific content */}
         </div>
     );
 }
