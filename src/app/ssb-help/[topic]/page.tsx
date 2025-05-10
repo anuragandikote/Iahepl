9466945dcaf3c6392dce963ce02020d9568bd2d0
anurag_chidanand <agandikote@gmail.com>
2025-05-10 16:38:47 +0530

    some progress

--- src/app/ssb-help/[topic]/page.tsx
+++ src/app/ssb-help/[topic]/page.tsx
@@ -1,19 +1,9 @@
-export const dynamic = 'force-dynamic';
+import React from 'react'
 
-type Props = {
-    params: {
-        topic: string;
-    };
-    searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default async function SSBHelpPage({ params }: Props) {
-    return (
-        <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold text-red-900 mb-6">
-                {decodeURIComponent(params.topic).replace(/-/g, ' ')}
-            </h1>
-            {/* Add SSB help topic specific content */}
-        </div>
-    );
+const SSBHelpPage = () => {
+  return (
+    <div>SSBHelpPage</div>
+  )
 }
+
+export default SSBHelpPage
